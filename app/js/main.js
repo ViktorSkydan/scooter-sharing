4ee9bf813afe2f8adc53c66d358b3a5369613145
Viktor Skydan <skydan.victor@gmail.com>
2023-06-19 16:48:09 +0300

    Start project

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+// document.querySelector('.button').focus({
+// 	border-radius: 4px,
+// 	outline: 2px solid rgba(73, 85, 103, 0.7)
+// });
+// document.querySelector('button').blur();
+
+// let btn = document.querySelector('button');
+// let link = document.querySelector('link');
+
+// btn.addEventListener('mouseup', function (e) {
+// 	console.log(btn.style);
+// 		btn.style.outline = 'transparent';
+// 		btn.style.outline = 'auto';
+// })
